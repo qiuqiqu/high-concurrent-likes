e54e20fa5eb60684ed86bfd035c6a17b2ff0bf11
yun.guo <1961237983@qq.com>
2025-04-22 16:35:37 +0800

    项目构建

---
+++ src/main/java/com/gy/DataTransApplication.java
@@ -0,0 +1,41 @@
+package com.gy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * @author yun.guo
+ * @date 2024/12/24
+ */
+@EnableScheduling
+@SpringBootApplication
+@Slf4j
+public class DataTransApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication application = new SpringApplication(DataTransApplication.class);
+            application.setBannerMode(Banner.Mode.OFF);
+            application.addListeners(pidFileWriter());
+            application.run(args);
+            String logo = "\n (♥◠‿◠)ﾉﾞ  服务启动成功   ლ(´ڡ`ლ)ﾞ  \n";
+            log.info(logo);
+        } catch (Exception e) {
+            log.error("", e);
+        }
+    }
+
+    /**
+     * 打印pid
+     */
+    public static ApplicationPidFileWriter pidFileWriter() {
+        String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+        return new ApplicationPidFileWriter("./pids/" + jvmName.split("@")[0] + ".pid");
+    }
+}
