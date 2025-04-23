83b892ea57fa1e99a836ada964171ae1b2d506a4
yun.guo <1961237983@qq.com>
2025-04-23 17:58:43 +0800

    knife4j

--- src/main/java/com/gy/controller/BlogController.java
+++ src/main/java/com/gy/controller/BlogController.java
@@ -6,6 +6,8 @@ import com.gy.common.ResultUtils;
 import com.gy.model.entity.Blog;
 import com.gy.model.vo.BlogVO;
 import com.gy.service.BlogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import java.util.List;
 /**
  * @author gy
  */
+@Tag(name = "博客")
 @RestController
 @RequestMapping("blog")
 public class BlogController {
@@ -24,12 +27,14 @@ public class BlogController {
     @Resource
     private BlogService blogService;
 
+    @Operation(summary = "获取博客")
     @GetMapping("/get")
     public BaseResponse<BlogVO> get(long blogId, HttpServletRequest request) {
         BlogVO blogVO = blogService.getBlogVOById(blogId, request);
         return ResultUtils.success(blogVO);
     }
 
+    @Operation(summary = "获取博客list")
     @GetMapping("/list")
     public BaseResponse<List<BlogVO>> list(HttpServletRequest request) {
         List<Blog> blogList = blogService.list();
--- src/main/java/com/gy/controller/ThumbController.java
+++ src/main/java/com/gy/controller/ThumbController.java
@@ -5,6 +5,8 @@ import com.gy.common.BaseResponse;
 import com.gy.common.ResultUtils;
 import com.gy.model.dto.thumb.DoThumbRequest;
 import com.gy.service.ThumbService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * @author gy
  */
+@Tag(name = "点赞")
 @RestController
 @RequestMapping("thumb")
 public class ThumbController {
@@ -23,12 +26,14 @@ public class ThumbController {
     @Resource
     private ThumbService thumbService;
 
+    @Operation(summary = "点赞")
     @PostMapping("/do")
     public BaseResponse<Boolean> doThumb(@RequestBody DoThumbRequest doThumbRequest, HttpServletRequest request) {
         Boolean success = thumbService.doThumb(doThumbRequest, request);
         return ResultUtils.success(success);
     }
 
+    @Operation(summary = "取消点赞")
     @PostMapping("/undo")
     public BaseResponse<Boolean> undoThumb(@RequestBody DoThumbRequest doThumbRequest, HttpServletRequest request) {
         Boolean success = thumbService.undoThumb(doThumbRequest, request);
