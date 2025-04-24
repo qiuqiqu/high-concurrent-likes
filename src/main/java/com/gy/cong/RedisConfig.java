fa37e0f6b4243b0c6e8b0eeff4b68ea5e962cbb4
yun.guo <1961237983@qq.com>
2025-04-24 10:24:06 +0800

    引入 Spring Session 和 redis

---
+++ src/main/java/com/gy/cong/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.gy.cong;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // 使用 GenericJackson2JsonRedisSerializer 序列化值
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+
+        // Key 使用 String 序列化
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public GenericJackson2JsonRedisSerializer springSessionDefaultRedisSerializer() {
+        // 让 Spring Session 使用 JSON 方式存储
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+}
